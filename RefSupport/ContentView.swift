e9f98de7174c080d75415c1ff16146efe9d7db6b
Shakebuddin <mohammed.shak@northeastern.edu>
2024-06-11 02:43:32 +0530

    Initial Commit

---
+++ RefSupport/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RefSupport/RefSupportApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefSupportApp.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct RefSupportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
