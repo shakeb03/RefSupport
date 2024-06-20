c1a5629c64163a29581ebb9e51663332f8ad2286
Shakebuddin <mohammed.shak@northeastern.edu>
2024-06-20 18:13:06 +0530

    Added card views for Home Screen Resources and function to fetch resources links from firebase

---
+++ RefSupport/Components/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-19.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ RefSupport/Model/Resource.swift
@@ -0,0 +1,8 @@
+//
+//  Resource.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-19.
+//
+
+import Foundation
