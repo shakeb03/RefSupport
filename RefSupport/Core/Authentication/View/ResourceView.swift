81cdc0cdcda882f81f3c740eb0081303c9a55e7d
Shakebuddin <mohammed.shak@northeastern.edu>
2024-06-24 23:13:35 +0530

    Navigation with Resources and Updates view configured

---
+++ RefSupport/Core/Authentication/View/ResourceView.swift
@@ -0,0 +1,18 @@
+//
+//  ResourceView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-24.
+//
+
+import SwiftUI
+
+struct ResourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResourceView()
+}
---
+++ RefSupport/Core/Authentication/View/UpdatesView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdatesView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-24.
+//
+
+import SwiftUI
+
+struct UpdatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdatesView()
+}
