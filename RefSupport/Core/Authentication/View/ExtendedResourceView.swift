10d5a64104680b162c0a77ac9a8b6f79a5df3fa4
Shakebuddin <mohammed.shak@northeastern.edu>
2024-06-28 16:15:47 +0530

    Added Edit Profile button in settings, Added Extended Resources View to categorize resource links

---
+++ RefSupport/Core/Authentication/View/ExtendedResourceView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtendedResourceView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-28.
+//
+
+import SwiftUI
+
+struct ExtendedResourceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtendedResourceView()
+}
