82733a27676b0cf08dda3a90e4b453c94bdd22a7
Shakebuddin <mohammed.shak@northeastern.edu>
2024-06-28 23:43:49 +0530

    updates page configured

---
+++ RefSupport/Core/Authentication/View/ExtendedUpdatesView.swift
@@ -0,0 +1,18 @@
+//
+//  ExtendedUpdatesView.swift
+//  RefSupport
+//
+//  Created by Shakeb . on 2024-06-28.
+//
+
+import SwiftUI
+
+struct ExtendedUpdatesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtendedUpdatesView()
+}
